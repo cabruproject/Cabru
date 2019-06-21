e4557239401a83ade24af61845ae1f04149bd505
Manoel Carvalho Neto <manoel.carvalho.neto@gmail.com>
2019-06-21 08:25:38 -0300

    Update dao.py

--- aula5/dao.py
+++ aula5/dao.py
@@ -18,7 +18,7 @@ class AlunoDao:
         if (aluno.id_aluno):
             cursor.execute(SQL_ATUALIZA_ALUNO, (aluno.nome, aluno.sexo, aluno.telefone, aluno.email. aluno.data_nascimento, aluno.data_matricula, aluno.turma_fk, aluno.desconto, aluno.id_aluno))
         else:
-            cursor.execute(SQL_CRIA_ALUNO, (aluno.nome, aluno.sexo, aluno.telefone, aluno.email. aluno.data_nascimento, aluno.data_matricula, aluno.turma_fk, aluno.desconto, aluno.id_aluno))
+            cursor.execute(SQL_CRIA_ALUNO, (aluno.nome, aluno.sexo, aluno.telefone, aluno.email. aluno.data_nascimento, aluno.data_matricula, aluno.turma_fk, aluno.desconto))
             aluno.id_aluno = cursor.lastrowid
         self.__db.connection.commit()
         return aluno
@@ -33,7 +33,7 @@ class AlunoDao:
         cursor = self.__db.connection.cursor()
         cursor.execute(SQL_ALUNO_POR_ID, (id,))
         tupla = cursor.fetchone()
-        return Aluno ( tupla[1], tupla[2], tupla[3], tupla[4], tupla[5], tupla[6], tupla[7], tupla[8], id_aluno =  tupla[0])
+        return Aluno ( tupla[1], tupla[2], tupla[3], tupla[4], tupla[5], tupla[6], tupla[7], tupla[8], tupla[0])
 
     def deletar(self, id):
         self.__db.connection.cursor().execute(SQL_DELETA_ALUNO, (id,))
@@ -54,7 +54,7 @@ class UsuarioDao:
 
 def traduz_alunos(alunos):
     def cria_aluno_com_tupla(tupla):
-        return Aluno( tupla[1], tupla[2], tupla[3], tupla[4], tupla[5], tupla[6], tupla[7], tupla[8], id_aluno = tupla[0])
+        return Aluno( tupla[1], tupla[2], tupla[3], tupla[4], tupla[5], tupla[6], tupla[7], tupla[8], tupla[0])
     return list(map(cria_aluno_com_tupla, alunos))
 
 
